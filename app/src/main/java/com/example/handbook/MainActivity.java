8a6460f37c4aae37147fffefb5b80e7b15fc2a31
luminorena <bakhuta.oa@gmail.com>
2022-09-16 16:49:39 +0300

    First Step: creating Main Activity and menus: menu_mushrooms, menu_flowers,menu_insects,menu_animals

---
+++ app/src/main/java/com/example/handbook/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.handbook;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //private ActivityMainBinding binding;
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //  binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+       // setContentView(binding.getRoot());
+       // DrawerLayout drawer = binding.drawerLayout;
+      //  NavigationView navigationView = binding.navView;
+      //  navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_mushrooms) {
+            Toast.makeText(this, "nav_mushrooms  pressed", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.nav_flowers) {
+            Toast.makeText(this, "nav_flowers pressed", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.nav_insects) {
+            Toast.makeText(this, "nav_insects pressed", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.nav_animals) {
+            Toast.makeText(this, "nav_animals pressed", Toast.LENGTH_SHORT).show();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
