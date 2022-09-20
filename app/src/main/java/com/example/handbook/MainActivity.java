10916d8fcd7264b235b8ec904625044ce6aa9832
luminorena <bakhuta.oa@gmail.com>
2022-09-20 19:11:01 +0300

    #4 Content samples added to the menu

--- app/src/main/java/com/example/handbook/MainActivity.java
+++ app/src/main/java/com/example/handbook/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.handbook;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,13 +16,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
         private DrawerLayout drawer;
+        private ListView list;
+        private String[] elementsArray;
+        private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = findViewById(R.id.listView);
+        elementsArray = getResources().getStringArray(R.array.mushroomsArray);
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1
+                , new ArrayList<String>(Arrays.asList(elementsArray)));
+        list.setAdapter(adapter);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
@@ -45,15 +61,46 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
         if (id == R.id.nav_mushrooms) {
             Toast.makeText(this, "nav_mushrooms  pressed", Toast.LENGTH_SHORT).show();
+            elementsArray = getResources().getStringArray(R.array.mushroomsArray);
+            adapter.clear();
+            adapter.addAll(elementsArray);
+            adapter.notifyDataSetChanged();
         }
         else if (id == R.id.nav_flowers) {
             Toast.makeText(this, "nav_flowers pressed", Toast.LENGTH_SHORT).show();
+            elementsArray = getResources().getStringArray(R.array.flowersArray);
+            adapter.clear();
+            adapter.addAll(elementsArray);
+            adapter.notifyDataSetChanged();
         }
         else if (id == R.id.nav_insects) {
             Toast.makeText(this, "nav_insects pressed", Toast.LENGTH_SHORT).show();
+            elementsArray = getResources().getStringArray(R.array.insectsArray);
+            adapter.clear();
+            adapter.addAll(elementsArray);
+            adapter.notifyDataSetChanged();
         }
         else if (id == R.id.nav_animals) {
             Toast.makeText(this, "nav_animals pressed", Toast.LENGTH_SHORT).show();
+            elementsArray = getResources().getStringArray(R.array.animalsArray);
+            adapter.clear();
+            adapter.addAll(elementsArray);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.nav_recipes) {
+            Toast.makeText(this, "nav_recipes pressed", Toast.LENGTH_SHORT).show();
+            elementsArray = getResources().getStringArray(R.array.recipesArray);
+            adapter.clear();
+            adapter.addAll(elementsArray);
+            adapter.notifyDataSetChanged();
+        }
+
+        else if (id == R.id.nav_advice) {
+            Toast.makeText(this, "nav_advice pressed", Toast.LENGTH_SHORT).show();
+            elementsArray = getResources().getStringArray(R.array.adviceArray);
+            adapter.clear();
+            adapter.addAll(elementsArray);
+            adapter.notifyDataSetChanged();
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
